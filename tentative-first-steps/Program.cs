8c4a285e04537506a8edbbd166d1c6d7d74220df
Tom Hooper <tomwbhooper@hotmail.com>
2021-08-12 16:56:05 +1200

    initial commit

---
+++ tentative-first-steps/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tentative_first_steps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tentative First Steps...");
+        }
+    }
+}
