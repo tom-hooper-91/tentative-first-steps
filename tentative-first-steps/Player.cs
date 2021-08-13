4ee4b68edf70a7aee6a743025864f76d8eb8a2fe
Tom Hooper <tomwbhooper@hotmail.com>
2021-08-13 12:23:41 +1200

    add script to start method

--- tentative-first-steps/Player.cs
+++ tentative-first-steps/Player.cs
@@ -3,6 +3,9 @@ namespace tentative_first_steps
 {
     public class Player
     {
-        
+        public string name;
+        public int hp = 10;
+        public int dmg = 1;
+        public int ac = 0;
     }
 }
--- tentative-first-steps/Program.cs
+++ tentative-first-steps/Program.cs
@@ -4,6 +4,8 @@ namespace tentative_first_steps
 {
     class Program
     {
+        public static Player currentPlayer = new Player(); // creates a new Player object which is an instance of the Player class
+
         static void Main(string[] args)
         {
             Program.Start();
@@ -13,8 +15,22 @@ namespace tentative_first_steps
         {
             Console.WriteLine("Tentative First Steps...");
             Console.Write("Player Name: ");
-            Console.ReadLine();
+            currentPlayer.name = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("A cold sensation wakes you. Your eyes are shut and your ears are ringing.");
+            Console.WriteLine("You can't remember where you are, or how you got here...");
+
+            if (currentPlayer.name == "")
+                Console.WriteLine("You can't even remember your own name...");
+            else
+                Console.WriteLine($"You only remember that your name is {currentPlayer.name}");
+
             Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("You blink your eyes open and are greeted by darkness.");
+            Console.WriteLine("You are lying on a cold stone floor.");
+            Console.WriteLine("You pull yourself upright and begin to feel your surroundings");
+            Console.WriteLine("After a few minutes you find a sturdy wooden door and push it open");
         }
     }
 }
